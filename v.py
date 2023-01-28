7169074190b596aad40e4d9d1c5ae7a6b3c26146
Cijin Joseph John <cijinjosephjohn007@gmail.com>
2023-01-28 17:01:36 +0530

    Changed v.py

--- v.py
+++ v.py
@@ -12,23 +12,24 @@ img = cv2.imread("back.jpg")
 
 image = cv2.resize(img,(640,480))
 
-# u_green = np.array([104,153,70])
 
-# l_green = np.array([30,30,0])
-
+u_green = np.array([104,153,70])
+l_green = np.array([30,30,0])
 # #highlighted green to white
-# mask = cv2.inRange(frame,l_green,u_green)
+
 
 # #and operation between mask and frame
 # #syntax (dest,src,mask)
 
-# res = cv2.bitwise_and(frame,frame,mask=mask)
+
 
 
 while(True):
     ret, frame = video.read()
-    cv2.imshow('frame',frame)
     frame = cv2.resize(frame,(640,480))
+    mask = cv2.inRange(frame,l_green,u_green)
+    res = cv2.bitwise_and(frame,frame,mask=mask)
+    cv2.imshow('frame',res)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         # cv2.destroyAllWindows()
